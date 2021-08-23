b8e8d01e1b0bae1b6767ece4661cb45050dccf44
PronDmytro <67123804+PronDmytro@users.noreply.github.com>
2021-08-23 13:41:54 +0300

    Assigment 10

--- src/com/shpp/p2p/cs/dpron/assignment10/Assignment10Part1.java
+++ src/com/shpp/p2p/cs/dpron/assignment10/Assignment10Part1.java
@@ -1,243 +1,204 @@
 package com.shpp.p2p.cs.dpron.assignment10;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.HashMap;
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
 
+import static java.lang.Double.parseDouble;
 
 public class Assignment10Part1 {
-    /**/
-    private static final String OP_ADD = "+";
-    private static final String OP_SUB = "-";
-    private static final String OP_MUL = "*";
-    private static final String OP_DIV = "/";
-    private static final String OP_EXP = "^";
+    /**
+     * HashMap which contains the key - the name of the variable, the value - the value of the variable.
+     */
+    static HashMap<String, Double> variables = new HashMap<>();
 
-    /**/
-    private static final String OPERATORS = OP_EXP + OP_MUL + OP_DIV + OP_SUB + OP_ADD;
-//    private static final String OPERATORS = "*/^+-";
-
-
+    /**
+     * The main class that serves as the entry point into the program.
+     * Initializes the parameter check and displays the result obtained during the calculation.
+     *
+     * @param args Arguments passed by the user.
+     */
     public static void main(String[] args) {
-
-        /* print expression*/
-        System.out.println("Expression: " + args[0]);
-        /* perform expression parsing */
-        Expressions expressionObj = new Expressions(args[0]);
-        if (expressionObj.isExpressionValid()) {
-            /* expression parsed successfully*/
-            ArrayList<String> expression = expressionObj.getExpression();
-            System.out.println(expression);
-            /* perform variables  parsing */
-            Variables variables = new Variables(args);
-            System.out.println(variables.getVariables());
-            System.out.println("Result: " + calculate(expression, variables));
+        if (args.length == 0) {
+            System.out.println("You have not entered any parameters.");
         } else {
-            System.out.println("Can't make calculation");
+            checkParameters(args);
+            System.out.println(calculate(args[0].replaceAll("\\s", ""), variables));
         }
     }
 
     /**
-     * Method to perform calculation of given expression
-     * with  input arguments
+     * A method that verifies the correctness of user input.
      *
-     * @param splittedExpression expression as ArrayList<String>
-     * @param vars               object of parsed variables
-     * @return String result of calculation
+     * @param args Arguments passed by the user.
      */
-    private static String calculate(ArrayList<String> splittedExpression, Variables vars) {
-        if (splittedExpression.size() == 1) {
-            return parseValue(splittedExpression.get(0), vars) + "";
-        } else
-            while (splittedExpression.size() > 1) {
-                for (int i = 0; i < OPERATORS.length(); i++) {
-                    String op = OPERATORS.charAt(i) + "";
-                    int index = getOperatorIndex(op, splittedExpression);
-                    while (index >= 0) {
-                        Double a = parseValue(splittedExpression.get(index - 1), vars);
-                        Double b = parseValue(splittedExpression.get(index + 1), vars);
-                        if (a != null && b != null) {
-                            switch (op) {
-                                case "^" -> splittedExpression.set(index - 1, (Math.pow(a, b)) + "");
-                                case "*" -> splittedExpression.set(index - 1, (a * b) + "");
-                                case "/" -> {
-                                    if (b == 0) return "Division by zero";
-                                    splittedExpression.set(index - 1, (a / b) + "");
-                                }
-                                case "-" -> splittedExpression.set(index - 1, (a - b) + "");
-                                case "+" -> splittedExpression.set(index - 1, (a + b) + "");
-                            }
-                            splittedExpression.remove(index);
-                            splittedExpression.remove(index);
-
-                        } else return "Can't make calculation";
-                        index = getOperatorIndex(op, splittedExpression);
-                    }
+    private static void checkParameters(String[] args) {
+        for (int i = 1; i < args.length; i++) {
+            // check for two characters ==
+            int count = 0;
+            for (int j = 0; j < args[i].length(); j++) {
+                if (args[i].charAt(j) == '=') {
+                    count++;
                 }
             }
-        return splittedExpression.get(0);
-    }
 
-    /**
-     * Method to find index of position of inputted operator
-     *
-     * @param operator String of one char(represent operator)
-     * @param list     ArrayList<String> to search in
-     * @return int searched operator index
-     */
-    static int getOperatorIndex(String operator, ArrayList<String> list) {
-
-        for (int index = 0; index < list.size(); index++) {
-            if (list.get(index).equals(operator)) {
-                return index;
-            }
-        }
-        return -1;
-    }
-
-    /**
-     * Method parses current string to Double
-     * if it is not possible tries fo find current string as argument in map of variables
-     *
-     * @param value String content to parse
-     * @param vars  object containing map of variables
-     * @return Double result of parsing (null, if not parsed and argument not declared)
-     */
-    static Double parseValue(String value, Variables vars) {
-        Double result = null;
-        try {
-            result = Double.parseDouble(value);
-        } catch (Exception e) {
-            if (vars.getVariables() != null && vars.getVariables().containsKey(value)) {
-                result = vars.getVariables().get(value);
+            // check that the argument does not begin and end with the = sign
+            if (args[i].indexOf('=') == -1 ||
+                    args[i].indexOf('=') == args[i].length() - 1 ||
+                    args[i].indexOf('=') == 0 ||
+                    count > 1) {
+                System.out.println("Invalid parameter : " + args[i]);
             } else {
-                System.out.println("Variable " + value + " not found");
+                // remove all spaces
+                args[i] = args[i].trim();
+                // everything before = in key everything after = in the number
+                String key = args[i].substring(0, args[i].indexOf('='));
+                String number = args[i].substring(args[i].indexOf('=') + 1);
+                // to whom we replace with a point
+                number = number.replace(',', '.');
+                // check that in key did not put numbers and in value - letters
+                if (!key.matches("[0-9]+") && number.matches("[0-9.-]+")) {
+                    variables.put(key, parseDouble(number));
+                } else {
+                    System.out.println("Invalid parameter : " + args[i]);
+                }
             }
         }
-        return result;
-    }
-}
-
-
- class Expressions {
-    /* array list of parsed expression*/
-    private ArrayList<String> splittedExpression;
-    /* true if expression parsed without errors*/
-    private static boolean valid = true;
-
-    /* Constructor */
-    public Expressions(String expression) {
-        splittedExpression = parseExpression(expression);
-        valid = splittedExpression != null;
     }
 
     /**
-     * Get parsed expression
-     * @return ArrayList<String> of parsed expression
-     */
-    public ArrayList<String> getExpression() {
-        return splittedExpression;
-    }
-
-    /**
-     * Method to parse input string to array list of operators an operands
-     * @param expression String to be parsed
-     * @return ArrayList<String> of parsed expression
-     */
-    private static ArrayList<String> parseExpression(String expression) {
-
-        /*delete spaces*/
-        expression = expression.replaceAll(" ", "");
-
-        /* split in lexemes like operator-operand (12+, aa*, a^)*/
-        if (expression.charAt(0) == '-') { // if first argument is negative
-            expression = "0" + expression;
-        }
-        Pattern pattern = Pattern.compile("[a-z0-9.\\s]+[/+*^-]");
-        Matcher matcher = pattern.matcher(expression);
-        int last = 0;
-        ArrayList<String> lexems = new ArrayList<>();
-        while (matcher.find()) {
-            last = matcher.end();
-            lexems.add(expression.substring(matcher.start(), last));
-        }
-        /* add last operand*/
-        String a = expression.substring(last);
-        lexems.add(a.replaceAll("[^a-z0-9.\\s]", ""));
-
-
-        /* check expression correctness*/
-        StringBuilder tmp = new StringBuilder();
-        for (String s : lexems) {
-            tmp.append(s);
-            if (!expression.contains(tmp)) {
-                System.out.println("Error in expression before " + s);
-                return null;
-            }
-        }
-
-        /* generate splited expression by operators and operands*/
-        ArrayList<String> temporary = new ArrayList<>();
-        for (int i = 0; i < lexems.size() - 1; i++) {
-            String tmpString = lexems.get(i).trim();
-            temporary.add(tmpString.substring(0, tmpString.length() - 1).trim());
-            temporary.add(tmpString.substring(tmpString.length() - 1).trim());
-        }
-        temporary.add(lexems.get(lexems.size() - 1).trim());
-        return temporary;
-
-    }
-
-    /**
-     * Get parsed expression status
-     * @return boolean parsed expression status (true if expression parse without errors)
-     */
-    public boolean isExpressionValid() {
-        return valid;
-    }
-}
-
-class Variables {
-    /*  Map of variables values*/
-    private static HashMap<String, Double> variables;
-
-    /* Constructor */
-    public Variables(String[] args) {
-        if(args.length > 1) {
-            variables = parseVariables(args);
-        }
-        else {
-            variables = null;
-            System.out.println("Nothing to parse for variables");
-        }
-    }
-
-    /*******************************************************************************************************************
+     * The function that enters the calculation formula and the variables
+     * that are set by the user and returns the calculated value.
      *
-     * @return parsed map of variables values
+     * @param formula   the formula to be calculated
+     * @param variables variables that are set by the user
+     * @return the value obtained
      */
-    public HashMap<String, Double> getVariables() {
-        return variables;
-    }
+    static double calculate(String formula, HashMap<String, Double> variables) {
+        //ArrayList which contains each formula element separately
+        ArrayList<String> arraySplit = new ArrayList<>();
 
-    /*******************************************************************************************************************
-     * Method to parse variables
-     * @param args array of strings with expected varibles values
-     * @return map of parsed variables
-     */
-    private static HashMap<String, Double> parseVariables(String[] args) {
-        HashMap<String, Double> vars = new HashMap<>();
-        /* begin parse from index 1 (0 - expression)*/
-        for (int i = 1; i < args.length; i++) {
-            try {
-                String[] words = args[i].split("=");
-                vars.put(words[0].trim(), Double.parseDouble(words[1]));
-            } catch (Exception e) {
-                /* can't split or parse double*/
-                System.out.println("Can't parse argument " + args[i]);
+        // check that the formula does not begin and end with a sign
+        if (theFirstSign(formula) > 0 && theFirstSign(formula.substring(formula.length() - 1)) == -1) {
+            while (theFirstSign(formula) != -1) {
+                arraySplit.add(formula.substring(0, theFirstSign(formula)));
+                formula = formula.substring(theFirstSign(formula));
+                arraySplit.add(formula.substring(0, 1));
+                formula = formula.substring(1);
+            }
+            arraySplit.add(formula);
+        } else {
+            System.out.println("Invalid formula entered : " + formula);
+        }
+
+        //replace the names of variables with their values
+        for (int i = 0; i < arraySplit.size(); i++) {
+            if (arraySplit.get(i).matches("[a-zA-Z][0-9]*+")) {
+                if (variables.get(arraySplit.get(i)) == null) {
+                    System.out.println("There is no such variable : " + arraySplit.get(i));
+                    System.exit(1);
+                }
+                arraySplit.set(i, Double.toString(variables.get(arraySplit.get(i))));
             }
         }
-        return vars;
+
+        return conductingMathematicalOperations(arraySplit);
+    }
+
+    /**
+     * A function that takes an ArrayList with a formula broken down into elements,
+     * calculates it, and returns the resulting value.
+     *
+     * @param arraySplit the formula is broken down into elements
+     * @return the value we get after the calculations
+     */
+    private static double conductingMathematicalOperations(ArrayList<String> arraySplit) {
+
+        //elevate to the power
+        for (int i = arraySplit.size() - 1; i > 0; i--) {
+            if (arraySplit.get(i).equals("^")) {
+                double res = Math.pow(parseDouble(arraySplit.get(i - 1)), parseDouble(arraySplit.get(i + 1)));
+                arraySplit.add(i - 1, Double.toString(res));
+                arraySplit.remove(i);
+                arraySplit.remove(i);
+                arraySplit.remove(i);
+            }
+        }
+
+        // multiply and divide
+        for (int i = 0; i < arraySplit.size() - 1; i++) {
+            if (arraySplit.get(i).equals("*") || arraySplit.get(i).equals("/")) {
+                if (arraySplit.get(i).equals("*")) {
+                    double res = parseDouble(arraySplit.get(i - 1)) * parseDouble(arraySplit.get(i + 1));
+                    arraySplit.add(i - 1, Double.toString(res));
+                    arraySplit.remove(i);
+                    arraySplit.remove(i);
+                    arraySplit.remove(i);
+                    i = i - 1;
+                } else if (arraySplit.get(i).equals("/")) {
+                    double res = parseDouble(arraySplit.get(i - 1)) / parseDouble(arraySplit.get(i + 1));
+                    arraySplit.add(i - 1, Double.toString(res));
+                    arraySplit.remove(i);
+                    arraySplit.remove(i);
+                    arraySplit.remove(i);
+                    i = i - 1;
+                }
+            }
+        }
+
+        // add and subtract
+        for (int i = 0; i < arraySplit.size() - 1; i++) {
+            if (arraySplit.get(i).equals("+") || arraySplit.get(i).equals("-")) {
+                if (arraySplit.get(i).equals("+")) {
+                    double res = parseDouble(arraySplit.get(i - 1)) + parseDouble(arraySplit.get(i + 1));
+                    arraySplit.add(i - 1, Double.toString(res));
+                    arraySplit.remove(i);
+                    arraySplit.remove(i);
+                    arraySplit.remove(i);
+                    i = i - 1;
+                } else if (arraySplit.get(i).equals("-")) {
+                    double res = parseDouble(arraySplit.get(i - 1)) - parseDouble(arraySplit.get(i + 1));
+                    arraySplit.add(i - 1, Double.toString(res));
+                    arraySplit.remove(i);
+                    arraySplit.remove(i);
+                    arraySplit.remove(i);
+                    i = i - 1;
+                }
+            }
+        }
+        return parseDouble(arraySplit.get(0));
+    }
+
+    /**
+     * A function that receives a formula at the input and returns the index of the very first character.
+     *
+     * @param formula the formula to be calculated
+     * @return index of the first character
+     */
+    private static int theFirstSign(String formula) {
+        int[] arrayIndexesOfCharacters = definitionOfIndices(formula);
+
+        int min = -1;
+        for (int number : arrayIndexesOfCharacters) {
+            if (number >= 0) {
+                min = number;
+                break;
+            }
+        }
+        return min;
+    }
+
+    /**
+     * The function that inputs the formula to be calculated, and returns an ordered array of character indices.
+     *
+     * @param formula the formula to be calculated
+     * @return an ordered array of character indices
+     */
+    private static int[] definitionOfIndices(String formula) {
+        int[] arrayIndexesOfCharacters = {formula.indexOf('^'), formula.indexOf('+'), formula.indexOf("-"),
+                formula.indexOf('*'), formula.indexOf('/')};
+        Arrays.sort(arrayIndexesOfCharacters);
+
+        return arrayIndexesOfCharacters;
     }
 }
